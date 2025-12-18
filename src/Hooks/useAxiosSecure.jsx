07395b33668cacd6bdf7f9b993f22baa66407052
sumaiyakhatun11 <sumaiyameghla1111@gmail.com>
2025-12-18 18:53:02 +0600

    slight modifications

---
+++ src/Hooks/useAxiosSecure.jsx
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../Provider/AuthProvider";
+
+
+const axiosSecure = axios.create({
+    baseURL: 'http://localhost:3000',
+
+})
+
+const useAxiosSecure = () => {
+    const { user } = useContext(AuthContext);
+    useEffect(() => {
+        const requestInterceptor = axiosSecure.interceptors.request.use(config => {
+            config.headers.Authorization = `Bearer ${user?.axiosToken}`;
+            return config;
+        });
+
+        const responseInterceptor = axiosSecure.interceptors.response.use(
+            response => response,
+            (error) => {
+                console.log(error);
+
+                return Promise.reject(error);
+            }
+        );
+        return () => {
+            axiosSecure.interceptors.request.eject(requestInterceptor);
+            axiosSecure.interceptors.response.eject(responseInterceptor);
+        };
+
+
+    }, [user]);
+
+    return axiosSecure;
+}
+
+export default useAxiosSecure;
